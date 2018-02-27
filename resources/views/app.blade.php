acb9ecea39bc28b0585dc357141169955348d418
inok94 <rodiknikolaev@yadndex.ru>
2018-02-27 14:24:34 +0300

    Views

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Dummy site</title>
-
+    <title>Test Alyce site</title>
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="msapplication-TileColor" content="#ffffff">
@@ -25,23 +24,17 @@
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 {{--    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">--}}
 
-
 </head>
-
 <body>
 
 @yield('content')
-
 
         <!-- Scripts -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="{{ url('/js/bootstrap.min.js') }}"></script>
 <script src="{{ url('/js/jquery-ui.js') }}"></script>
-
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <script src="{{ url('/js/ie10-viewport-bug-workaround.js') }}"></script>
-
-
 <script>
     var site_url = "{{ url('/') }}";
 
@@ -50,9 +43,6 @@
             'X-CSRF-Token': "{{ csrf_token() }}"
         }
     });
-
 </script>
-
-
 </body>
 </html>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 
-
     <div class="navbar navbar-default ">
         <div class="container">
             <div class="navbar-header">
@@ -11,7 +10,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#"><b>SITE</b></a>
+                <a class="navbar-brand" href="/"><b>SITE</b></a>
             </div>
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
@@ -19,42 +18,45 @@
             </div>
         </div>
     </div>
-
     <div class="container">
-
         <div class="col-md-6">
-
             <h3>Users</h3>
             <ul class="list-group">
                 @foreach( $users as $user )
                     <li class="list-group-item">
-                        {{ $user->name }} has {{ $user->getApplesCount() }} apples <a class="btn btn-xs btn-danger pull-right" href="{{ url('/take-apple/'.$user->id) }}">Grab apple</a>
+                        {{ $user->name }} has {{ $user->getApplesCount() }}
+                        apples {{ $user->getApplesCount() === 1 ? '' : 's' }}{{ $user->getApplesCount() === 0 ? '' : ':' }}
+                        <div class="pull-left">
+                            @if (count($user->storages) > 0)
+                                <ul>
+                                    @foreach( $user->storages as $i => $order )
+                                        <li>Apple #{{ $order->apple_id }} grabbed
+                                            at {{ date('M. j, Y H:i:s T', strtotime($order->created_at)) }}{{ $i < count($user->storages) - 1 ? ', ' : '.' }}</li>
+                                    @endforeach
+                                </ul>
+                            @endif
+                        </div>
+                        <a class="btn btn-xs btn-default pull-right" href="{{ url('/take-apple/'.$user->id) }}">Grab
+                            apple</a>
+                        <div class="clearfix"></div>
                     </li>
                 @endforeach
             </ul>
-
             <div class="centered">
-                <a class="btn btn-lg btn-success text-center" href="{{ url('/free-apples') }}">Free Apples</a>
+                <a class="btn btn-lg btn-success text-center" href="{{ url('/free-apples') }}">Reset Apples</a>
             </div>
-
         </div>
-
-
         <div class="col-md-6">
-
             <h3>Basket</h3>
             <ul class="list-group">
                 @foreach( $basketApples as $apple )
                     <li class="list-group-item">
-                        Apple
+                        Apple <span class="pull-right text-muted">#{{ $apple->id }}</span>
                     </li>
                 @endforeach
             </ul>
-
         </div>
-
     </div>
-
 
 @endsection
 
